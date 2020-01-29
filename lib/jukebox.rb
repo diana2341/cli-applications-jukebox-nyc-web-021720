5d301404912da92f9730f627ff32ab45ff6a1065
Diana Ponce <dianap2341@gmail.com>
2020-01-29 02:53:41 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -62,13 +62,14 @@ def play(songs)
   def run (songs)
     puts "Please enter a command:"
    user_input = gets.strip
+   if user_input== "exit"
+     exit_jukebox
       if user_input =="help"
     help
   elsif user_input == "list"
   list(songs) 
   elsif user_input =="play"
       end
-      exit_jukebox
   end
 
 
